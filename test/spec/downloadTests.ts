fa83922be3e878dd36397d26dbc2685fc7bd4637
matthewkeil <me@matthewkeil.com>
2024-12-09 20:30:57 +0700

    test: add spec tests

---
+++ test/spec/downloadTests.ts
@@ -0,0 +1,11 @@
+import {downloadTests} from "@lodestar/spec-test-util/downloadTests";
+import {ethereumConsensusSpecsTests} from "./specTestVersioning.js";
+
+/* eslint-disable no-console */
+
+for (const downloadTestOpts of [ethereumConsensusSpecsTests]) {
+  downloadTests(downloadTestOpts, console.log).catch((e: Error) => {
+    console.error(e);
+    process.exit(1);
+  });
+}
